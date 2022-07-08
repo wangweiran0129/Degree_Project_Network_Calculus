cc3a8389108c3daf2953554829d8f8d8dcb3cb5f
wangweiran0129 <wangwr0129@gmail.com>
2022-07-08 13:38:12 +0200

    Added delayBoundCalculation4OneFoi in the NetCal4Python.java which meets the case that there is only one foi in the topology

--- DNC/src/main/java/org/networkcalculus/dnc/degree_project/NetCal4Python.java
+++ DNC/src/main/java/org/networkcalculus/dnc/degree_project/NetCal4Python.java
@@ -25,8 +25,6 @@ public class NetCal4Python {
     public double[] delayBoundCalculation(double[] server_rate, double[] server_latency, double[] flow_rate,
             double[] flow_burst, int[] flow_src, int[] flow_dest, int[] foi_id) {
 
-        System.out.println("hello ?");
-
         /*
          * This method will calculate the delay bounds based on the network features and
          * the flow(s) of interest
@@ -159,6 +157,150 @@ public class NetCal4Python {
                 System.out.println("delay bound double : " + pmoo_delay_bound[i]);
             }
 
+        } catch (Exception e) {
+            System.out.println("PMOO Analysis Failed");
+            e.printStackTrace();
+        }
+
+        return pmoo_delay_bound;
+
+    }
+
+    public double delayBoundCalculation4OneFoi(double[] server_rate, double[] server_latency, double[] flow_rate,
+            double[] flow_burst, int[] flow_src, int[] flow_dest, int foi_id) {
+
+        /*
+         * This method will calculate the delay bounds based on the network features and
+         * one flow of interest. I don't know how to overwrite the Java method, so I
+         * created another Java method to calculate the delay bound for the topology
+         * where there is only one flow of interest inside. It's almost the same with
+         * the method above except there is only flow of interest in this method
+         * 
+         * @param server_rate: the rate of each server in this topology
+         * 
+         * @param server_latency: the latency of each server in this topology
+         * 
+         * @param flow_rate: the rate of each flow in this topology
+         * 
+         * @param flow_burst: the burst of each flow in this topology
+         * 
+         * @param flow_src: the source server of each flow
+         * 
+         * @param flow_dest: the destination/sink server of each flow
+         * 
+         * @param foi_id: the flow(s) of interest in this topology
+         * 
+         * @return: the delay bound based on the network features and the designated
+         * flow of interest
+         */
+
+        int server_number = server_rate.length;
+        int flow_number = flow_rate.length;
+        System.out.println("# servers : " + server_number);
+        System.out.println("# flows : " + flow_number);
+        System.out.println();
+
+        ServerGraph sg = new ServerGraph();
+        AnalysisConfig configuration = new AnalysisConfig();
+        configuration.enforceMaxSC(AnalysisConfig.MaxScEnforcement.GLOBALLY_ON);
+        configuration.enforceMaxScOutputRate(AnalysisConfig.MaxScEnforcement.GLOBALLY_ON);
+
+        // Define the service curve and add the service curves to the servers
+        ServiceCurve[] service_curve = new ServiceCurve[server_number];
+        Server[] servers = new Server[server_number];
+        for (int i = 0; i < server_number; i++) {
+            service_curve[i] = Curve.getFactory().createRateLatency(server_rate[i], server_latency[i]);
+            servers[i] = sg.addServer(service_curve[i]);
+        }
+
+        // Figure out the server order
+        String server_order = "";
+        int order_increase_counter = 0;
+        int order_decrease_counter = 0;
+        for (int i = 0; i < flow_number; i++) {
+            if (flow_src[i] <= flow_dest[i]) {
+                order_increase_counter++;
+            }
+            if (flow_src[i] >= flow_dest[i]) {
+                order_decrease_counter++;
+            }
+        }
+
+        if (order_increase_counter == flow_number) {
+            server_order = "increasing";
+        }
+        if (order_decrease_counter == flow_number) {
+            server_order = "decreasing";
+        }
+        if (order_increase_counter != flow_number && order_decrease_counter != flow_number) {
+            System.out.println("Chaotic Server Connections. Delay Bounds Cannot Be Calculated");
+            System.exit(0);
+        }
+
+        System.out.println("The order of this topology : " + server_order);
+        System.out.println();
+
+        // Connect the servers
+        try {
+            if (server_order.equals("increasing")) {
+                for (int i = 0; i < server_number - 1; i++) {
+                    sg.addTurn(servers[i], servers[i + 1]);
+                }
+            }
+            if (server_order.equals("decreasing")) {
+                for (int i = server_number - 1; i > 0; i--) {
+                    sg.addTurn(servers[i], servers[i - 1]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Server Connections Error");
+            e.printStackTrace();
+        }
+
+        // Define the arrival curves
+        ArrivalCurve[] arrival_curve = new ArrivalCurve[flow_number];
+        try {
+            for (int i = 0; i < flow_number; i++) {
+                arrival_curve[i] = Curve.getFactory().createTokenBucket(flow_rate[i], flow_burst[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Arrival Curve Adding Error");
+            e.printStackTrace();
+        }
+
+        // Add the arrival curves to the flows
+        try {
+            for (int i = 0; i < flow_number; i++) {
+                int src = flow_src[i];
+                int dest = flow_dest[i];
+                if (src == dest) {
+                    sg.addFlow(arrival_curve[i], servers[src]);
+                } else {
+                    sg.addFlow(arrival_curve[i], servers[src], servers[dest]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Flow Adding Error");
+            e.printStackTrace();
+        }
+
+        double pmoo_delay_bound = 0;
+
+        try {
+            // Define the flow of interest
+            Flow flow_of_interest = sg.getFlow(foi_id);
+            System.out.println("flow of interest : " + flow_of_interest);
+            System.out.println();
+
+            // Analyze the network
+            System.out.println("--- PMOO Analysis ---");
+            System.out.println();
+            PmooAnalysis pmoo = new PmooAnalysis(sg, configuration);
+            pmoo.performAnalysis(flow_of_interest);
+            Num pmoo_db = pmoo.getDelayBound();
+            pmoo_delay_bound = pmoo_db.doubleValue();
+            System.out.println("delay bound Num : " + pmoo_db);
+            System.out.println("delay bound double : " + pmoo_delay_bound);
         } catch (Exception e) {
             System.out.println("PMOO Analysis Failed");
             e.printStackTrace();
