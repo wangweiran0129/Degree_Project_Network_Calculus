43d10212e93f1bbcdc2e64edf1aaac98d42c424c
wangweiran0129 <wangwr0129@gmail.com>
2022-08-22 18:35:49 +0200

    Add prediction on the attacked network and change the name of the prediction script on the original network

---
+++ DeepFP_gnn-main/src/output/pdatnw.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --chdir /home/weirwang/Degree_Project_Network_Calculus/DeepFP_gnn-main/src/output/
+
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 1
+#SBATCH --ntasks 1
+#SBATCH --mem 48G
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu_free
+#SBATCH --gres=gpu:1
+#SBATCH --time 06:00:00
+
+echo Running on `hostname`
+
+source /home/weirwang/venvs/izar-python/bin/activate
+
+java -jar ../NetCal.jar &
+MY_JAVA_PID=$!
+python -m predict_attacked_networks "../model/deepfpPMOO.pt" "../../../Network_Information_and_Analysis/attacked_topology/before_fp/"
+kill $MY_JAVA_PID
+exit 0
